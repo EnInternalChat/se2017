c94329b5e71244a693f5dedc17606028826cb708
lxs <lxs137@hotmail.com>
2017-05-01 01:59:50 +0800

    task page

--- app/src/pages/task-list/task-list.ts
+++ app/src/pages/task-list/task-list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { HTTPService } from '../../providers/http_helper';
+import { Task } from './task';
 /**
  * Generated class for the TaskList page.
  *
@@ -14,11 +16,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TaskList {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  task_status_array = ['tasks_not_done', 'tasks_done'];
+  task_status : string = this.task_status_array[0];
+
+  tasks_list_not_done : Array<Task>;
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public web_helper: HTTPService) {
+    this.web_helper.get("assets/data/tasks.json", null).then(
+      (res) => {
+        this.tasks_list_not_done = res;
+        console.log(this.tasks_list_not_done);
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TaskList');
   }
 
+  swipe_event(event) {
+    if(event.direction == 2) {
+      // 向左滑
+      if(this.task_status_array.indexOf(this.task_status) == 0) {
+        this.task_status = this.task_status_array[1];
+      }
+    }
+    else if(event.direction == 4) {
+      // 向右滑
+      if(this.task_status_array.indexOf(this.task_status) == 1) {
+        this.task_status = this.task_status_array[0];
+      }
+    }
+  }
 }
---
+++ app/src/pages/task-list/task.ts
@@ -0,0 +1,7 @@
+export class Task {
+
+  constructor(public id : string, 
+              public content: string,
+              public start_person: string,
+              public update_time: string) {}
+}
