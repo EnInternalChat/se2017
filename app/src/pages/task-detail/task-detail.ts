566313d66a3b66172b4e41c3aa4716f611503262
lxs <lxs137@hotmail.com>
2017-05-04 17:33:50 +0800

    stage line complete

--- app/src/pages/task-detail/task-detail.ts
+++ app/src/pages/task-detail/task-detail.ts
@@ -19,10 +19,15 @@ export class TaskDetail {
 
   public task_info : Task;
   public user_name : string;
+  public operation_options : Array<{value : string, text : string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     let global_data = AppGlobal.get_instance();
     this.task_info = navParams.data.task;
     this.user_name = global_data.user_name;
+    this.operation_options = [
+      {"value": "yes", "text": "同意"},
+      {"value": "no", "text": "驳回"}
+    ];
     // console.log("detail: ", this.task_info.stages[0]);
   }
 
--- app/src/providers/task.ts
+++ app/src/providers/task.ts
@@ -1,47 +1,52 @@
 export class TaskStage {
-  public finishTime : string;
+  public finish_time : string;
   public person : Array<string>;
   public content : string;
   public time_day : string;
   public time_hour : string;
   public person_str : string;
 
+  public is_pass : boolean = true;
+
   public constructor(json: any) {
-    this.finishTime = json["finishTime"];
+    this.finish_time = json["finishTime"];
     this.person = json["person"];
     this.content = json["content"];
-    let times = this.finishTime.split(" ");
+    let times = this.finish_time.split(" ");
     if(times.length >= 2) {
       this.time_day = times[0];
       this.time_hour = times[1];
     }
     else {
-      this.time_day = this.finishTime;
+      this.time_day = this.finish_time;
       this.time_hour = "";
     }
     if(this.person != undefined)
       this.person_str = this.person.join(",");
+    else
+      this.person_str = " ";
   } 
 
 }
 
 export class Task {
-  public activityID : string;
+  public activity_id : string;
   public content : string;
-  public startPerson : string;
-  public startTime : string;
-  public updateTime : string;
+  public start_person : string;
+  public start_time : string;
+  public update_time : string;
   public stages : Array<TaskStage> = [];
 
   constructor(json: any) {
-    this.activityID = json["activityID"];
+    this.activity_id = json["activityID"];
     this.content = json["content"];
-    this.startPerson = json["startPerson"];
-    this.startTime = json["startTime"];
-    this.updateTime = json["updateTime"];
+    this.start_person = json["startPerson"];
+    this.start_time = json["startTime"];
+    this.update_time = json["updateTime"];
     let stages_json = json["stages"];
     for(let i = 0, n = stages_json.length; i < n; i++)
       this.stages.push(new TaskStage(stages_json[i]));
+    this.stages[this.stages.length - 1].is_pass = false;
   }
 
 }
